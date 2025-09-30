673fc506a1015a57fa1af1b9859897208f0dbf74
Ven <veneleys@gmail.com>
2025-09-30 17:36:18 +0300

    Added bitwise function, improved Hardnum

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+
 class Utils:
     nums = "0123456789"
     letters = "abcdefghijklmnopqrstuvwxyz"
     numsLetters = nums+letters
     class HardNum:
-        def __init__(self, arg) -> None:
+        def __init__(self, arg, currentSystem:int = 10) -> None:
             self.num = []
             self.ishard = False
+            self.currentSystem = currentSystem
+
+            if isinstance(arg, Utils.HardNum):
+                self.num = arg.num
+                return
 
             if isinstance(arg, int):
                 self.num = [int(i) for i in str(arg)]
@@ -53,17 +59,7 @@ class Utils:
         def __len__(self):
             return len(self.num)
         
-        def __makeEasyNum(self):
-            if self.ishard:
-                raise IndexError("Can`t make easy num due this num is hard")
-            num = ""
-            for i in self.num:
-                num += Utils.numsLetters[i]
-            return num
-        
         def __str__(self):
-            if not(self.ishard):
-                return self.__makeEasyNum()
             num = ""
             for i in self.num:
                 if i > 36:
@@ -72,51 +68,78 @@ class Utils:
                     i = Utils.numsLetters[i]
                 num += str(i)
             return num
-        def __iter__(self):
+        def __iter__(self) -> iter:
             return iter(self.num)
         
-        def __getitem__(self, key):
+        def __getitem__(self, key) -> int:
             return self.num[key]
         
-        def __int__(self):
-            return int(self.__makeEasyNum())
+        def __getRealMinSystem(self) -> int:
+            return max(self.num)+1
+        
+        def __int__(self) -> int:
+            if self.currentSystem == 10 and self.__getRealMinSystem() <= 10:
+                return int(self.__str__())
+            raise ValueError("Unable convert number to int: use getTen or systemToTen instead")
+            
+        def getCurrentSystem(self, *args) -> int:
+            return self.currentSystem
+        
+        def getTen(self, currentSystem: int = None):
+            if not currentSystem:
+                currentSystem = self.currentSystem
+            if currentSystem == 10 and self.__getRealMinSystem() <= 10:
+                return self
+            if currentSystem == 10 and self.__getRealMinSystem() > 10:
+                raise ValueError("Unable convert number to base10: real number base is unknown")
+            return Utils.systemToTen(self, currentSystem)
+        
+        def getSystemFromCurrentSystem(self, endSystem: int):
+            if self.currentSystem == 10 and self.__getRealMinSystem() <= 10:
+                return Utils.tenToSystem(self, endSystem)
+            if self.currentSystem == 10 and self.__getRealMinSystem() > 10:
+                raise ValueError(f"Unable convert number to base{endSystem}: real number base is unknown")
+            return Utils.systemToSystem(self, self.currentSystem, endSystem)
+        
+        def getSystemFromSystem(self, startSystem: int, endSystem: int, updateCurrentSystem: bool = False):
+            if updateCurrentSystem:
+                self.currentSystem = startSystem
+            return Utils.systemToSystem(self, startSystem, endSystem)
 
-    def tenToSystem(num: int, system: int, returnStr = True) -> str:
-        if isinstance(num,str):
-            num = int(num)
-        ans: str = ""
-
+    def tenToSystem(num, system: int) -> HardNum:
+        num = int(Utils.HardNum(num))
+        
         if system > 36:
+            ans: list = []
             while num > 0:
                 a = num%system
                 num //= system
-                if a > 36:
-                    a = f'_{a}_'
-                else:
-                    a = Utils.numsLetters[a]
-                ans = a + ans
-            return Utils.HardNum(ans)
 
+                ans = [a] + ans
+            return Utils.HardNum(ans, system)
+
+        ans: str = ""
         while num > 0:
             ans = Utils.numsLetters[num%system] + ans
             num //= system
-        if returnStr:
-            return ans
-        for i in Utils.letters:
-            if i in ans:
-                raise ValueError(f"can`t convert number with base {system} containing literals to int")
-        return int(ans)
+        return Utils.HardNum(ans, system)
     
-    def systemToTen(num: str, system: int, returnStr: bool = False) -> str:
-        if not(isinstance(num, Utils.HardNum)):
-            num = Utils.HardNum(num)
+    def systemToTen(num, system: int) -> HardNum:
+        num = Utils.HardNum(num, system)
         ans = 0
         l = len(num)
         for index, i in enumerate(num):
             if i >= system:
-                raise ValueError(f"invalid number with base {system}: containing a number ({i}) larger or equal base")
+                raise ValueError(f"invalid number with base {system}: containing a number ({i}) equal or larger base")
             ans += i*system**(l-1-index)
     
-        if returnStr:
-            return int(ans)
-        return ans
+        return Utils.HardNum(ans)
+    
+    def systemToSystem(num, startSystem: int, endSystem: int):
+        num = Utils.systemToTen(num, startSystem)
+        num = Utils.tenToSystem(num, endSystem)
+        return num
+    
+    def bitwise(num1, num2):
+        num1, num2 = int(Utils.HardNum(num1)), int(Utils.HardNum(num2))
+        return Utils.HardNum(num1 & num2)
